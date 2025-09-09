4bcce343e92c7d3930f750208fa61955c424e7e6
gayatri2004 <gayatrisanap2572004@gmail.com>
2025-09-09 16:20:30 +0530

    Adding some arrays problem-solutions

---
+++ Arrays/Missingele.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Missingele{
+    public static int Missing_ele(int[]arr){
+        int n = arr.length+1;
+        long Total_sum =((long) n*(n+1))/2;
+        long curr_sum =0;
+        for(int i=0;i<arr.length;i++){
+            curr_sum+=arr[i];
+        }
+        return (int) (Total_sum-curr_sum);
+    }
+    public static void main(String[]args){
+        int[] arr = {1,2,3,4};
+        System.out.print("Current Missing element  is : " + Missing_ele(arr));
+    }
+}
