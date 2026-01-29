a6ba68598f3f6932c4e75354122493bb1c7b4d9f
gayatri2004 <gayatrisanap2572004@gmail.com>
2026-01-29 21:48:56 +0530

    new updates

--- Practice/pr.java
+++ Practice/pr.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import java.util.*;
 public class pr{
     public static void main(String[]args){
@@ -18,3 +19,58 @@ public class pr{
       
     
     }
+=======
+// import java.util.*;
+// public class pr{
+//     public static void main(String[]args){
+//       int arr1 [] = {1,2,4,3,5};
+//       int n = arr1.length+1;
+//       int sum_of_n =( n*(n+1))/2;
+//       int sum =0;
+//       for(int i=0;i<arr1.length;i++){
+//          sum += arr1[i];
+//       }
+//       int missing_element = sum_of_n - sum;
+//       System.out.print("missing_element is : " + missing_element);
+//     }
+// }
+// import java.util.*;
+// public class pr{
+//     public static void main(String[]args){
+//         int arr1 [] = {1,2,4,33,50};
+//         int largest_element = Integer.MIN_VALUE;
+//         int second_element = Integer.MIN_VALUE;
+//         for(int i =0;i<arr1.length;i++){
+//             if(largest_element<arr1[i]){
+//               second_element = largest_element;
+//               largest_element = arr1[i];
+//             }
+//             else if(arr1[i]>second_element && largest_element>arr1[i]){
+//                 second_element = arr1[i];
+//             }
+//         }
+//         System.out.println("Largest is " + largest_element + " Second largest is " + second_element);
+//     }
+// }
+// import java.util.*;
+// public class pr{
+//     public static void main(String[]args){
+//         int [] arr1 = {1, 2, 3, 4, 5};
+//         boolean result = true;
+//         for(int i=0;i<arr1.length-1;i++){
+//             if(arr1[i]>arr1[i+1]){
+//                   result = false;
+//             }
+//         }
+//         System.out.println(result);
+//     }
+// }
+// Find duplicate element
+import java.util.*;
+public class pr{
+    public static void main(String[]args){
+        int [] arr1 = {1, 3, 4, 2, 2};
+        
+    }
+}
+>>>>>>> 3dc3bd6 (new updates)
--- pr.java
+++ pr.java
@@ -1,20 +1,18 @@
 import java.util.*;
 public class pr{
-    public static void main(String[]args){
-       int [] arr1 = {10,30,20,60,50,100};
-      int largest_num = Integer.MIN_VALUE;
-      int second_largest = Integer.MIN_VALUE;
-      for(int i=0;i<arr1.length;i++){
-          if(arr1[i]>largest_num){
-            second_largest = largest_num;
-             largest_num=arr1[i];
-          }else if (second_largest<arr1[i] && arr1[i]!= largest_num){
-            second_largest=arr1[i];
-          }
-        }
-        System.out.println("Largest Number is : " + largest_num);
-        System.out.println("second_largest is : " + second_largest);
-      }
-      
-    
+  public static void main(String[]args){
+    int [] arr1 = {1, 2, 3, 2, 1,4};
+    int [] arr2 = new int[arr1.length];
+    int end = arr1.length-1;
+    for(int i =0;i<arr1.length/2;i++){
+      int temp = arr1[end];
+      arr2[end]= arr1[i];
+      arr2[i]=temp;
+      end --;
     }
+    for(int i=0;i<arr2.length;i++){
+      System.out.print(arr2[i] + " ");
+    }
+    
+  }
+}
