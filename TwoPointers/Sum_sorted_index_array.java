7860a21996e50a57909e879849bb6156d65ba270
gayatri2004 <gayatrisanap2572004@gmail.com>
2026-03-18 10:35:57 +0530

    Problems from two pointers

---
+++ TwoPointers/Sum_sorted_index_array.java
@@ -0,0 +1,41 @@
+// Example 1:
+
+// Input: numbers = [2,7,11,15], target = 9
+// Output: [1,2]
+// Explanation: The sum of 2 and 7 is 9. Therefore, index1 = 1, index2 = 2. We return [1, 2].
+// Example 2:
+
+// Input: numbers = [2,3,4], target = 6
+// Output: [1,3]
+// Explanation: The sum of 2 and 4 is 6. Therefore index1 = 1, index2 = 3. We return [1, 3].
+
+
+import java.util.*;
+public class Sum_sorted_index_array{
+    public static int[] arrfun(int[]arr,int target){
+       int start =0;
+       int end = arr.length-1;
+       while(start<end){
+        int sum = arr[start]+arr[end];
+        if(sum==target){
+            return new int[]{start+1,end+1};
+        }
+        if(sum>target){
+            end--;
+        }else{
+            start++;
+        }
+       }
+       return new int[]{};
+    }
+    public static void main(String[]args){
+        int arr[] = {1,2,3,7};
+        int target = 5;
+        System.out.print(Arrays.toString(arrfun(arr,target)));
+    }
+}
+
+
+
+
+
